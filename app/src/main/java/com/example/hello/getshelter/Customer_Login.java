0ec13656ad0a276711ed44297ac719cea873296d
Abhishek Ner <abhiner@users.noreply.github.com>
2019-09-17 15:11:08 +0530

    customer Login

--- app/src/main/java/com/example/hello/getshelter/Customer_Login.java
+++ app/src/main/java/com/example/hello/getshelter/Customer_Login.java
@@ -1,10 +1,15 @@
 package com.example.hello.getshelter;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Customer_Login extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,17 @@ public class Customer_Login extends AppCompatActivity {
         ActionBar actionBar= getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.ic_launcher);
+
+        textView=(TextView)findViewById(R.id.txt_view);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Customer_Login.this,Customer_SignUp.class);
+                startActivity(intent);
+
+
+                Toast.makeText(Customer_Login.this, "Register Here", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hello/getshelter/Owner_SignUp.java
@@ -0,0 +1,13 @@
+package com.example.hello.getshelter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Owner_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner__sign_up);
+    }
+}
