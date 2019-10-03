c4368e44f389963c0f0a95b57a86963eb02ce8d0
vinodbadgujar <vbadgujar1999@gmail.com>
2019-10-03 19:10:09 +0530

    icons on customer and owner page

--- app/src/main/java/com/example/hello/getshelter/CustomerPage.java
+++ app/src/main/java/com/example/hello/getshelter/CustomerPage.java
@@ -1,5 +1,6 @@
 package com.example.hello.getshelter;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class  CustomerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_page);
+        ActionBar actionBar= getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.ic_launcher);
     }
 }
